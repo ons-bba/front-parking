91b146c4dee40d275f05d94aa81cbbc2fefb7a6f
Abderraouf <jabli.abderraouf@gmail.com>
2025-05-31 00:47:48 +0100

    mise a jour

--- src/app/backoffice/parking-module/parking-form/parking-form.component.ts
+++ src/app/backoffice/parking-module/parking-form/parking-form.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 
 import { ParkingService } from './../services/parking.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Parking } from '../../../shared/interfaces/parking.interface';
@@ -20,83 +20,89 @@ import { Parking } from '../../../shared/interfaces/parking.interface';
   templateUrl: './parking-form.component.html',
   styleUrl: './parking-form.component.scss'
 })
-export class ParkingFormComponent implements OnInit {
-  parkingForm! : FormGroup;
-  parkingId : string | null = null
- 
- 
-  constructor(private fb:FormBuilder,
-    private parkingService:ParkingService,
-    private route:ActivatedRoute
-  ){}
+export class ParkingFormComponent  {
+onBack() {
+this.back.emit();
+}
 
-  ngOnInit(): void {
+  @Output() parkingCreated = new EventEmitter<Parking>();
+  @Output() back = new EventEmitter<void>()
+  parkingForm: FormGroup;
+  isLoading = false;
+  errorMessage = '';
 
- 
-     this.parkingForm = this.fb.group({
-      nom:[''],
-      statut:['FERME',Validators.required],
-      placesTotal : [null,Validators.minLength(0)],
-      placesDisponible:[null,Validators.minLength(0)],
-      horaires : this.fb.group({
-        ouverture : ['08:00', [Validators.required, Validators.pattern(/^([0-1][0-9]|2[0-3]):[0-5][0-9]$/)]],
-        fermeture:['20:00', [Validators.required, Validators.pattern(/^([0-1][0-9]|2[0-3]):[0-5][0-9]$/)]]
+  constructor(
+    private fb: FormBuilder,
+    private parkingService: ParkingService
+  ) {
+    this.parkingForm = this.fb.group({
+      nom: ['', [Validators.required, Validators.maxLength(100)]],
+      placesTotal: [1, [Validators.required, Validators.min(1)]],
+      statut: ['OUVERT'],
+      localisation: this.fb.group({
+        type: ['Point'],
+        coordinates: [[0, 0], Validators.required] // [longitude, latitude]
       }),
-      localisation : this.fb.group({
-        coordinates:this.fb.array([[0],[0]])
-      })
+      horaires: this.fb.group({
+        ouverture: ['08:00', Validators.pattern(/^([0-1][0-9]|2[0-3]):[0-5][0-9]$/)],
+        fermeture: ['20:00', Validators.pattern(/^([0-1][0-9]|2[0-3]):[0-5][0-9]$/)]
+      }),
+      prestations: [[]]
     });
-      this.parkingId =this.route.snapshot.paramMap.get('id')
-      
-      if(this.parkingId !== null){
-        this.loadParking(this.parkingId)
-      }
-
-    
-    
-
-    
   }
 
-  loadParking(id:string){
-    this.parkingService.getParkingById(id).subscribe({
-      next : (parking:Parking)=>{
-        console.log("the parking",parking)
-        this.parkingForm.patchValue({
-          nom : parking.nom,
-          statut:parking.statut,
-          placesTotal:parking.placesTotal,
-          placesDisponible:parking.placesDisponible,
-          horaires : parking.horaires,
-          localisation : parking.localisation.coordinates
-        });
-        console.log(this.parkingForm)
-      }
-    })
-  }
+  onSubmit(): void {
+    if (this.parkingForm.valid) {
+      this.isLoading = true;
+      const formData = {
+        ...this.parkingForm.value,
+        placesDisponible: this.parkingForm.value.placesTotal // Initialize available spots
+      };
 
-  // getLongitude(){
-  //   const  
-  // }
-
- 
-
-
-
-
-  
-  
-  
-  
-  
-  onSubmit():void{
-    if(this.parkingForm.valid){
-      const data = this.parkingForm.value;
-      console.log("valeur de formulaire:",data)
-    }else {
-      console.log('Formulaire invalid')
+      this.parkingService.createParking(formData).subscribe({
+        next: (newParking) => {
+          this.parkingCreated.emit(newParking);
+          this.parkingForm.reset();
+          this.isLoading = false;
+        },
+        error: (err) => {
+          this.errorMessage = err.error?.message || 'Erreur lors de la création';
+          this.isLoading = false;
+        }
+      });
     }
   }
 
-  
+
+updateLongitude(event: Event): void {
+  const inputElement = event.target as HTMLInputElement;
+  const value = parseFloat(inputElement.value);
+  if (!isNaN(value)) {
+    const currentCoords = this.parkingForm.get('localisation.coordinates')?.value || [0, 0];
+    this.parkingForm.get('localisation.coordinates')?.setValue([value, currentCoords[1]]);
   }
+}
+
+updateLatitude(event: Event): void {
+  const inputElement = event.target as HTMLInputElement;
+  const value = parseFloat(inputElement.value);
+  if (!isNaN(value)) {
+    const currentCoords = this.parkingForm.get('localisation.coordinates')?.value || [0, 0];
+    this.parkingForm.get('localisation.coordinates')?.setValue([currentCoords[0], value]);
+  }
+}
+ 
+  }
+
+ 
+ 
+
+
+
+
+  
+  
+  
+  
+  
+ 
