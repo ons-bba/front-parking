a0be42310c0d46e95902f700f13ca69c4c5cf119
Abderraouf <jabli.abderraouf@gmail.com>
2025-05-31 02:14:59 +0100

    signals

--- src/app/backoffice/parking-module/parking-list/parking-list.component.ts
+++ src/app/backoffice/parking-module/parking-list/parking-list.component.ts
@@ -1,30 +1,49 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { Parking } from '../../../shared/interfaces/parking.interface';
 import { ParkingService } from '../services/parking.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { Parking } from '../../../shared/interfaces/parking.interface';
-
 
 @Component({
-  standalone :true,
-  selector: 'app-parking-list',
-  imports: [CommonModule,
-    RouterModule
-  ],
-  templateUrl: './parking-list.component.html',
-  styleUrl: './parking-list.component.scss'
+standalone :true,
+selector: 'app-parking-list',
+imports: [CommonModule,
+RouterModule
+],
+templateUrl: './parking-list.component.html',
+styleUrl: './parking-list.component.scss'
 })
-export class ParkingListComponent {
-  @Input() parkings: Parking[] = [];
-  @Output() selectParking = new EventEmitter<Parking>();
-  selectedId: string | null = null;
+export class ParkingListComponent implements OnInit{
+parkings : Parking[]=[];
+displayedColumns = ['nom','statut','places','horaires','actions'];
+isLoading = true;
+errorMessage ='';
 
-  onSelect(parking: Parking) {
-    this.selectedId = parking._id;
-    this.selectParking.emit(parking);
-  }
-  
+constructor(private parkingService : ParkingService){
 
+}
+ngOnInit(): void {
+this.loadParkings();
+}
 
+loadParkings():void {
+
+this.parkingService.getAllParkings().subscribe({
+next : (data)=>{this.parkings=data
+this.isLoading = false
+},
+error : (err)=>console.error("Erreur lors de la chargement",err)
+});
+
+}
+
+deleteParking(id:string):void {
+if(confirm('supprimer un parking?')){
+this.parkingService.deleteParking(id).subscribe({
+next : ()=>this.loadParkings(),
+error: (err)=> console.error('Erreur lors de la supprission',err)
+});
+}
+}
 }
